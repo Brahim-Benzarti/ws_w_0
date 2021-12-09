3d300f316aae7e4768504ada4d3ac4c5de9aafcb
Brahim Benzarti <benzartibrahim54@gmail.com>
2021-12-09 21:08:43 +0100

    front end done

---
+++ app/Http/Controllers/Emails.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Emails extends Controller
+{
+    public function sendCasual(Request $request){
+        dd($request);
+        
+    }
+}
